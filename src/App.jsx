687e36bbcfc74594972f63ea693af77fda7a52ad
Wilfred <wilyfrez@gmail.com>
2022-08-30 21:40:18 +0100

    created folder and file structure

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return <div className=" text-3xl underline uppercase">Go Helooword</div>;
+}
+
+export default App;
